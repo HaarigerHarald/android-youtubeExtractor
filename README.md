adf1def0feeeb1fb09c5fdc53fa5dc5724ab803f
HaarigerHarald <benjamin.huber@a1.net>
2015-02-10 15:26:16 +0100

    Rewrite Sample app as dialog

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-A small android based YouTube uri extractor.
+A small android based YouTube url extractor.
 =======================================================
 
-These are the literal uris to the YouTube video or audio files, so you can stream or download them.
+These are the literal urls to the YouTube video or audio files, so you can stream or download them.
 It features an age verification circumvention and a signature deciphering method (mainly for vevo videos).
 
 I've made a little jar lib which should make integration super easy: [youtubeExtractor.jar](https://github.com/HaarigerHarald/android-youtubeExtractor/raw/master/bin/youtubeExtractor.jar)
@@ -28,16 +28,16 @@ It's basically build around an AsyncTask. Called from an Activity you can write 
     ytEx.execute(youtubeLink);
 
 The important thing is the ytFiles SparseArray. Because YouTube videos are available in multiple formats we can choose one by
-calling ytFiles.get(itag). One ytFile contains the uri and its appropriate meta data like: "mp4 1280x720" or "m4a dash aac"
+calling ytFiles.get(itag). One ytFile contains the url and its appropriate meta data like: "mp4 1280x720" or "m4a dash aac"
 
-For further infos have a look at the supplied sample YoutubeDownloader app. It uses the "Share" function in the official Youtube
-app to download Youtube videos. It doesn't have a launcher entry though so don't be irritated.
+For further infos have a look at the supplied sample YoutubeDownloader app. It uses the "Share" function in the official YouTube
+app to download YouTube videos. It doesn't have a launcher entry though so don't be irritated.
 
 To try the app have a look at: [youtubeDownloader.apk](https://github.com/HaarigerHarald/android-youtubeExtractor/raw/master/bin/youtubeDownloader.apk)
 
-<img src='https://github.com/HaarigerHarald/android-youtubeExtractor/raw/master/Screenshot_2014-10-02-02-10-48.png' width='250' alt='youtubeDownloader Screenshot 1'>
+<img src='https://github.com/HaarigerHarald/android-youtubeExtractor/raw/master/Screenshot_2015-02-10-15-12-45.png' width='250' alt='youtubeDownloader Screenshot 1'>
 
-<img src='https://github.com/HaarigerHarald/android-youtubeExtractor/raw/master/Screenshot_2014-09-30-21-12-34.png' width='250' alt='youtubeDownloader Screenshot 2'>
+<img src='https://github.com/HaarigerHarald/android-youtubeExtractor/raw/master/Screenshot_2015-02-10-15-03-33.png' width='250' alt='youtubeDownloader Screenshot 2'>
 
 I'm using a useful lib for JavaScript execution, if you want to build your own lib from the sources head 
 over to: https://github.com/evgenyneu/js-evaluator-for-android
--- src/at/huber/sampleDownload/SampleDownloadActivity.java
+++ src/at/huber/sampleDownload/SampleDownloadActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,34 +15,39 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
-import at.huber.youtubeExtractor.R;
+import at.huber.sampleDownload.R;
 import at.huber.youtubeExtractor.YouTubeUriExtractor;
 import at.huber.youtubeExtractor.YtFile;
 
 public class SampleDownloadActivity extends Activity {
-	
+
 	LinearLayout mainLayout;
 	ProgressBar mainProgressBar;
 	Context activityContext;
 
+	private static String youtubeLink;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_sample_download);
-		mainLayout=(LinearLayout)findViewById(R.id.main_layout);
-		mainProgressBar=(ProgressBar)findViewById(R.id.prgrBar);
+		colorTitle(Color.parseColor("#E62117"));
+		mainLayout=(LinearLayout) findViewById(R.id.main_layout);
+		mainProgressBar=(ProgressBar) findViewById(R.id.prgrBar);
 		activityContext=this;
-		
-		//Check how it was started and if we can get the youtubelink somehow
+
+		// Check how it was started and if we can get the youtube link somehow
 		if (savedInstanceState == null && Intent.ACTION_SEND.equals(getIntent().getAction())
 				&& getIntent().getType() != null && "text/plain".equals(getIntent().getType())){
-			
-			String youtubeLink=getIntent().getStringExtra(Intent.EXTRA_TEXT);
 
-			if (youtubeLink != null && (youtubeLink.contains("http://youtu.be/") || 
-					youtubeLink.contains("youtube.com/watch?v="))){
+			String ytLink=getIntent().getStringExtra(Intent.EXTRA_TEXT);
+
+			if (ytLink != null
+					&& (ytLink.contains("http://youtu.be/") || ytLink.contains("youtube.com/watch?v="))){
+				youtubeLink=ytLink;
 				// We have a valid link such as: http://youtu.be/xxxx or
 				// http://youtube.com/watch?v=xxxx
 				getYoutubeDownloadUrl(youtubeLink);
@@ -49,49 +55,52 @@ public class SampleDownloadActivity extends Activity {
 				Toast.makeText(this, R.string.error_no_yt_link, Toast.LENGTH_LONG).show();
 				finish();
 			}
+		}else if (savedInstanceState != null && youtubeLink != null){
+			getYoutubeDownloadUrl(youtubeLink);
 		}else{
 			finish();
 		}
 	}
-	
+
 	private void getYoutubeDownloadUrl(String youtubeLink) {
 		YouTubeUriExtractor ytEx=new YouTubeUriExtractor(this) {
 
 			@Override
 			public void onUrisAvailable(String videoId, String videoTitle, SparseArray<YtFile> ytFiles) {
 				mainProgressBar.setVisibility(View.GONE);
-				
-				if(ytFiles==null){
-					//Something went wrong we got no links. Please always check this.
+
+				if (ytFiles == null){
+					// Something went wrong we got no urls. Please always check
+					// this.
 					finish();
 					return;
 				}
-				//Iterate over itags
+				// Iterate over itags
 				for(int i=0, itag=0; i < ytFiles.size(); i++){
 					itag=ytFiles.keyAt(i);
-					//ytFile represents one file with its uri and meta data
+					// ytFile represents one file with its url and meta data
 					YtFile ytFile=ytFiles.get(itag);
-					
-					//Ignore the google proprietary webm format
-					if(!ytFile.getMeta().getExt().equalsIgnoreCase("webm")){
-						//Just add videos in a decent format => height -1 = audio
-						if(ytFile.getMeta().getHeight()>0 && ytFile.getMeta().getHeight()<360){
-							continue;
-						}
+
+					// Just add videos in a decent format => height -1 = audio
+					if (ytFile.getMeta().getHeight() == -1 || ytFile.getMeta().getHeight() >= 360){
 						addButtonToMainLayout(videoTitle, ytFile);
-					}		
+					}
 				}
 			}
 		};
-		//Lets execute the request
+		// Ignore the google proprietary webm format
+		ytEx.setIncludeWebM(false);
+		// Lets execute the request
 		ytEx.execute(youtubeLink);
-		
+
 	}
-	
-	private void addButtonToMainLayout(final String videoTitle, final YtFile ytfile){
-		//Lets display some buttons to let the user choose the format he wants to download
+
+	private void addButtonToMainLayout(final String videoTitle, final YtFile ytfile) {
+		// Lets display some buttons to let the user choose the format he wants
+		// to download
+		String btnText=(ytfile.getMeta().getHeight() == -1) ? "Audio m4a" : ytfile.getMeta().getInfo();
 		Button btn=new Button(activityContext);
-		btn.setText(ytfile.getMeta().getInfo());
+		btn.setText(btnText);
 		btn.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -109,21 +118,35 @@ public class SampleDownloadActivity extends Activity {
 		});
 		mainLayout.addView(btn);
 	}
-	
-	private void downloadFromUrl(String youtubeDlUrl,String downloadTitle, String fileName) {
+
+	private void downloadFromUrl(String youtubeDlUrl, String downloadTitle, String fileName) {
 		Uri uri=Uri.parse(youtubeDlUrl);
-		DownloadManager.Request request = new DownloadManager.Request(uri);
+		DownloadManager.Request request=new DownloadManager.Request(uri);
 		request.setTitle(downloadTitle);
-		// in order for this if to run, you must use the android 3.2 to compile your app
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-		    request.allowScanningByMediaScanner();
-		    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+		// in order for this if to run, you must use the android 3.2 to compile
+		// your app
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+			request.allowScanningByMediaScanner();
+			request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
 		}
 		request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
 
 		// get download service and enqueue file
-		DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+		DownloadManager manager=(DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
 		manager.enqueue(request);
+	}
+
+	private void colorTitle(int color) {
+		try{
+			int dividerId=getResources().getIdentifier("android:id/titleDivider", null, null);
+			View divider=findViewById(dividerId);
+			divider.setBackgroundColor(color);
+			int textViewId=getResources().getIdentifier("android:id/title", null, null);
+			TextView tv=(TextView) findViewById(textViewId);
+			tv.setTextColor(color);
+		}catch (NullPointerException npe){
+			// Ignore something has changed
+		}
 	}
 
 }
