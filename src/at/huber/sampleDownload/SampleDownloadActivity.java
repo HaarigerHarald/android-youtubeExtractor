50aa35f7c5ac6f4b0fa212c52ef5b41f8d9a5715
HaarigerHarald <benjamin.huber@a1.net>
2015-04-18 18:37:02 +0200

    Simplify and shrink the Sample App

--- src/at/huber/sampleDownload/SampleDownloadActivity.java
+++ src/at/huber/sampleDownload/SampleDownloadActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.SparseArray;
@@ -17,17 +16,15 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import at.huber.sampleDownload.R;
 import at.huber.youtubeExtractor.YouTubeUriExtractor;
 import at.huber.youtubeExtractor.YtFile;
 
 public class SampleDownloadActivity extends Activity {
 
-	LinearLayout mainLayout;
-	ProgressBar mainProgressBar;
-	Context activityContext;
-
 	private static String youtubeLink;
+	
+	private LinearLayout mainLayout;
+	private ProgressBar mainProgressBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +34,8 @@ public class SampleDownloadActivity extends Activity {
 		colorTitle(Color.parseColor("#E62117"));
 		mainLayout=(LinearLayout) findViewById(R.id.main_layout);
 		mainProgressBar=(ProgressBar) findViewById(R.id.prgrBar);
-		activityContext=this;
 
-		// Check how it was started and if we can get the youtube link somehow
+		// Check how it was started and if we can get the youtube link
 		if (savedInstanceState == null && Intent.ACTION_SEND.equals(getIntent().getAction())
 				&& getIntent().getType() != null && "text/plain".equals(getIntent().getType())){
 
@@ -48,8 +44,7 @@ public class SampleDownloadActivity extends Activity {
 			if (ytLink != null
 					&& (ytLink.contains("://youtu.be/") || ytLink.contains("youtube.com/watch?v="))){
 				youtubeLink=ytLink;
-				// We have a valid link such as: http://youtu.be/xxxx or
-				// http://youtube.com/watch?v=xxxx
+				// We have a valid link
 				getYoutubeDownloadUrl(youtubeLink);
 			}else{
 				Toast.makeText(this, R.string.error_no_yt_link, Toast.LENGTH_LONG).show();
@@ -70,8 +65,7 @@ public class SampleDownloadActivity extends Activity {
 				mainProgressBar.setVisibility(View.GONE);
 
 				if (ytFiles == null){
-					// Something went wrong we got no urls. Please always check
-					// this.
+					// Something went wrong we got no urls. Always check this.
 					finish();
 					return;
 				}
@@ -96,10 +90,9 @@ public class SampleDownloadActivity extends Activity {
 	}
 
 	private void addButtonToMainLayout(final String videoTitle, final YtFile ytfile) {
-		// Lets display some buttons to let the user choose the format he wants
-		// to download
+		// Display some buttons and let the user choose the format
 		String btnText=(ytfile.getMeta().getHeight() == -1) ? "Audio m4a" : ytfile.getMeta().getInfo();
-		Button btn=new Button(activityContext);
+		Button btn=new Button(this);
 		btn.setText(btnText);
 		btn.setOnClickListener(new OnClickListener() {
 
@@ -111,7 +104,7 @@ public class SampleDownloadActivity extends Activity {
 				}else{
 					filename=videoTitle + "." + ytfile.getMeta().getExt();
 				}
-				filename=filename.replaceAll("\\\\|>|<|\"|\\||\\*|\\?|%|:", "");
+				filename=filename.replaceAll("\\\\|>|<|\"|\\||\\*|\\?|%|:|#", "");
 				downloadFromUrl(ytfile.getUrl(), videoTitle, filename);
 				finish();
 			}
@@ -123,12 +116,9 @@ public class SampleDownloadActivity extends Activity {
 		Uri uri=Uri.parse(youtubeDlUrl);
 		DownloadManager.Request request=new DownloadManager.Request(uri);
 		request.setTitle(downloadTitle);
-		// in order for this if to run, you must use the android 3.2 to compile
-		// your app
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
-			request.allowScanningByMediaScanner();
-			request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-		}
+		
+		request.allowScanningByMediaScanner();
+		request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
 		request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
 
 		// get download service and enqueue file
