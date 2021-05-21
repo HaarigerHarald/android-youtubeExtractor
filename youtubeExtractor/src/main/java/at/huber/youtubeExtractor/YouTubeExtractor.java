46579d12cb33ea9815720416a48bf5f5fb9c88b7
HaarigerHarald <haarigerharald@gmx.at>
2021-05-21 16:05:19 +0200

    Add html5=1 parameter

--- youtubeExtractor/src/main/java/at/huber/youtubeExtractor/YouTubeExtractor.java
+++ youtubeExtractor/src/main/java/at/huber/youtubeExtractor/YouTubeExtractor.java
@@ -206,7 +206,7 @@ public abstract class YouTubeExtractor extends AsyncTask<String, Void, SparseArr
     private SparseArray<YtFile> getStreamUrls() throws IOException, InterruptedException {
 
         String ytInfoUrl = (useHttp) ? "http://" : "https://";
-        ytInfoUrl += "www.youtube.com/get_video_info?video_id=" + videoID + "&eurl="
+        ytInfoUrl += "www.youtube.com/get_video_info?html5=1&video_id=" + videoID + "&eurl="
                 + URLEncoder.encode("https://youtube.googleapis.com/v/" + videoID, "UTF-8");
 
         String streamMap;
